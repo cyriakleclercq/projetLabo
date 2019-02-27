e70306b21c387fee55c9ecc0476f3db48e80064a
james <charlie592009@outlook.fr>
2019-02-27 14:30:48 +0100

    v1

---
+++ js/mouvement.js
@@ -0,0 +1,14 @@
+var perso = document.querySelector('"perso');
+var posInitial = {x:0, y:0};
+var pos = posInitial;
+var posFinal = {x:200, y:100};
+
+var dx = (posFinal.x - posInitial.x) / (posFinal.y - posInitial.y);
+var dy = (posFinal.y - posInitial.y) / (posFinal.x - posInitial.x);
+
+var interval = setInterval(function(){
+    if(pos.x > posFinal.x || pos.y > posFinal.y)clearInterval(interval);
+    pos.x+=dx; pos.y+=dy;
+    perso.style.left = pos.x +'px';
+    perso.style.top = pos.y + 'px';
+}, 5);
