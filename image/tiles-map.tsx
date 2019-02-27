a573c9c7be514f8478b6a894f92f13340d09b344
cyriakleclercq <cyriakleclercq@gmail.com>
2019-02-27 14:07:37 +0100

    commit initial V2

---
+++ image/tiles-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tiles-map" tilewidth="32" tileheight="32" tilecount="99" columns="11">
+ <image source="map/town_rpg_pack/graphics/tiles-map.png" width="352" height="288"/>
+</tileset>
---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var pseudo = document.getElementById('pseudo').value;
+
+$('#partie2').hide();
+
+$('#btStart').click(function () {
+
+    $('#partie2').show();
+    $('#partie1').hide();
+
+});
+
