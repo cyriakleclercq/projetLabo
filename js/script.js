95318826e5da309760c2364d281d65ddf76d6a05
cyriakleclercq <cyriakleclercq@gmail.com>
2019-02-28 13:21:54 +0100

    js changement de page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
-$('#start').hide();
+//$('#start').hide();
 $('#quest').hide();
+$('.map').hide();
 
 $('#btStart').click(function ()
 {
@@ -10,14 +11,36 @@ $('#btStart').click(function ()
 
 });
 
-for ($i=0; $i<=4; $i++)
+for (let $i=0; $i<=4; $i++)
 {
 
         $("#quest"+[$i]).click(function ()
         {
-                $('#quest').hide();
-                $('#map').show();
+
+                if ($i== 0) {
+
+                        $('#quest').hide();
+                        $('#start').hide();
+                        $('.map').show();
+                }
+
+                if($i == 1) {
+                        document.location.href = '../html/quête_labo.html';
+                }
+
+                if($i == 2) {
+                        document.location.href = '../html/quête_mediatheque.html';
+                }
+
+                if($i == 3) {
+                        document.location.href = '../html/ecomusee_quete.html';
+                }
+
+                if($i == 4) {
+                        document.location.href = '../html/maison-europe_quete.html';
+                }
         })
 }
+
 
 
