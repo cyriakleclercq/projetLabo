b1a35a3cb946865d376888b12d0e1fe129759514
cyriakleclercq <cyriakleclercq@gmail.com>
2019-03-01 12:18:42 +0100

    recup json en js

---
+++ js/json.js
@@ -0,0 +1,17 @@
+function ajaxRequest()
+{
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            var objQuete = this.responseText;
+
+        }
+
+        var quetes = JSON.parse(objQuete);
+
+    }
+}
