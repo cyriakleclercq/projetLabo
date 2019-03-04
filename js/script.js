1ecc9508bbb1675668cf5e3cb2e9b2855ef31eed
cyriakleclercq <cyriakleclercq@gmail.com>
2019-03-04 16:06:10 +0100

    resolution affichage map

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 $('#start').hide();
 $('#quest').hide();
-//$('#super_glo').hide();
 
 $('#btStart').click(function ()
 {
@@ -16,31 +15,44 @@ function random () {
 
         for (let $i = 0; $i <= 4; $i++) {
 
+
                 $ ("#quest" + [$i]).click (function () {
 
 
                         if ($i == 0) {
 
-                                $ ('#quest').hide ();
-                                $ ('#start').hide ();
+                                document.location.href = '../html/quête_mairie.html';
                                 $ ('#super_glo').show ();
+
 
                         }
 
                         if ($i == 1) {
                                 document.location.href = '../html/quête_labo.html';
+                                $ ('#super_glo').show ();
+
+
                         }
 
                         if ($i == 2) {
                                 document.location.href = '../html/quête_mediatheque.html';
+                                $ ('#super_glo').show ();
+
+
                         }
 
                         if ($i == 3) {
                                 document.location.href = '../html/ecomusee_quete.html';
+                                $ ('#super_glo').show ();
+
+
                         }
 
                         if ($i == 4) {
                                 document.location.href = '../html/maison-europe_quete.html';
+                                $ ('#super_glo').show ();
+
+
                         }
                 })
         }
