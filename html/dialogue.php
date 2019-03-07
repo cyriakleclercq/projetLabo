ae392e55cbbf5523433a85c41463a5dfb2aa0100
Geoffrey04 <43751762+Geoffrey04@users.noreply.github.com>
2019-03-07 11:38:32 +0100

    remplacement 5pages html vers 1page php

---
+++ html/dialogue.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../css/css2.css">
+</head>
+<body>
+<div id="global0">
+
+    <div id="avatar1"></div>
+
+    <div id="dial1">
+        <blockquote class="dial1">coucouuuuuu</blockquote>
+        <blockquote class="dial1">toiiiiiii</blockquote>
+        <blockquote class="dial1">bruhhhhhh</blockquote>
+        <blockquote class="dial1">pouloulou</blockquote>
+    </div>
+
+</div>
+
+
+<div id="global1">
+
+    <div id="avatar2"></div>
+
+    <div id="dial2">
+        <blockquote class="dial2">coucouuuuuu</blockquote>
+        <blockquote class="dial2">toiiiiiii</blockquote>
+        <blockquote class="dial2">bruhhhhhh</blockquote>
+        <blockquote class="dial2">pouloulou</blockquote>
+    </div>
+
+</div>
+
+
+<div id="global2">
+
+    <div id="avatar3"></div>
+
+    <div id="dial3">
+        <blockquote class="dial3">coucouuuuuu</blockquote>
+        <blockquote class="dial3">toiiiiiii</blockquote>
+        <blockquote class="dial3">bruhhhhhh</blockquote>
+        <blockquote class="dial3">pouloulou</blockquote>
+    </div>
+
+</div>
+
+
+<div id="global3">
+
+    <div id="avatar4"></div>
+
+    <div id="dial4">
+        <blockquote class="dial4">coucouuuuuu</blockquote>
+        <blockquote class="dial4">toiiiiiii</blockquote>
+        <blockquote class="dial4">bruhhhhhh</blockquote>
+        <blockquote class="dial4">pouloulou</blockquote>
+    </div>
+
+</div>
+
+
+<div id="global4">
+
+    <div id="avatar5"></div>
+
+    <div id="dial5">
+        <blockquote class="dial5">coucouuuuuu</blockquote>
+        <blockquote class="dial5">toiiiiiii</blockquote>
+        <blockquote class="dial5">bruhhhhhh</blockquote>
+        <blockquote class="dial5">pouloulou</blockquote>
+    </div>
+
+</div>
+
+
+
+<script src="../js/test.js"></script>
+</body>
+</html>
