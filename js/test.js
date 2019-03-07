3043955477f71f3361af034b1b1c05c15e24c78a
cyriakleclercq <cyriakleclercq@gmail.com>
2019-03-07 10:51:41 +0100

    MVM

--- js/test.js
+++ js/test.js
@@ -6,12 +6,40 @@ $perso.style.left = "52%";
 
 
 
+for(let $i =0; $i<=4; $i++) {
 
-$('#bouton').click(function () {
+    $('#check'+$i).click(function () {
 
-    $perso.style.top = '70%';
+        if($i == 0) {
+            $perso.style.top = '40%';
+            $perso.style.left = '50.5%';
+        }
+        if($i == 1) {
+            $perso.style.top = '80%';
+            $perso.style.left = '49.7%';
+        }
 
-})
+        if($i == 2) {
+            $perso.style.top = '91.3%';
+            $perso.style.left = '59.9%';
+        }
+
+        if($i == 3) {
+            $perso.style.top = '56%';
+            $perso.style.left = '23.3%';
+        }
+
+        if($i == 4) {
+            $perso.style.top = '88.7%';
+            $perso.style.left = '13%';
+        }
+
+
+
+    })
+
+}
+
 
 
 
