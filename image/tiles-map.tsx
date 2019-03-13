5e5e09e5fbce3b9ed43c3349bc908f6794c85090
Geoffrey04 <43751762+Geoffrey04@users.noreply.github.com>
2019-03-13 14:53:10 +0100

    modif_map

--- image/tiles-map.tsx
+++ image/tiles-map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="tiles-map" tilewidth="32" tileheight="32" tilecount="99" columns="11">
- <image source="map/town_rpg_pack/graphics/tiles-map.png" width="352" height="288"/>
+<tileset version="1.2" tiledversion="1.2.2" name="tiles-map" tilewidth="16" tileheight="16" tilecount="1400" columns="50">
+ <image source="test_map.png" width="801" height="449"/>
 </tileset>
